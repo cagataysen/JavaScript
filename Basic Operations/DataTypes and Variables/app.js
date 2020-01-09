dc1d91d49866c8a85c2f7fe7d8b42f9cc0d530f7
cagatay <drosen_26@hotmail.com>
2020-01-09 22:54:11 +0300

    https://github.com/cagataysen/JavaScript.git

---
+++ Basic Operations/DataTypes and Variables/app.js
@@ -0,0 +1,82 @@
+var a = 20; 
+
+var b = 10;
+
+var c = 40; 
+
+console.log(a,b,c);
+
+//Primitive
+
+// var a = 10;   //number datatype
+// var b = 3.14;
+// console.log(typeof a);
+// console.log(typeof b); 
+
+//Reference 
+
+// var name = "Cagatay";
+
+// console.log(name);
+// console.log(typeof name);
+
+// Boolean
+
+// var a = true;
+
+// console.log(typeof a);
+
+// var a = null;
+
+// console.log(a);
+
+// console.log(typeof a);
+
+// var a;
+
+// a = 20;
+
+// console.log(a);
+
+
+// var numbers = [1,2,3,4,5,6];
+
+// console.log(numbers);
+// console.log(typeof numbers);
+// console.log(numbers[0]);
+
+// var person = {
+//     name: "Cagatay Sen",
+//     age: 21
+// }
+// console.log(person);
+// console.log(typeof person);
+
+// var date = new Date();
+
+// console.log(date);
+
+// var hello = function() {
+//     console.log("hello");
+// }
+
+// console.log(hello);
+// console.log(typeof hello);
+
+// var a = 10;
+
+// var b = a;
+
+// console.log(a,b);
+
+// a = 20;
+
+// console.log(a,b);
+
+// var a = [1,2,3];
+
+// var b = a;
+
+// a.push(4);
+
+// console.log(b);
