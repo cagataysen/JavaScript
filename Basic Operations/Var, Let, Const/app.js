041232c6476df0b962b7f131f1a903683c102f55
cagatay <drosen_26@hotmail.com>
2020-01-10 02:02:11 +0300

    https://github.com/cagataysen/JavaScript.git

---
+++ Basic Operations/Var, Let, Const/app.js
@@ -0,0 +1,45 @@
+// var name = "Cagatay Sen";
+
+// var name = "Cagatay Sen";
+
+
+// console.log(name);
+
+// let name = "Cagatay Sen";  // 2 kere define edemiyosun var gibi, mesela
+//let name = "Cagatay" desek olmuyor
+
+// console.log(name);
+
+// console.log(name); 
+
+// let a,b;
+// a = 10;
+// b = 20;
+
+// console.log(a+b);
+
+// const name = "Cagatay Sen";
+// console.log(name);
+
+// name = "Cagatay"; //error verir
+// console.log(name);
+
+// const a;
+
+// a = 10; // error verir, const satırında tanımlıcaksın bunu sonra bişi diyemezsin
+// console.log(a); 
+
+const a = [1,2,3,4,5];
+
+console.log(a);
+
+a.push(6);  //this is ok
+
+console.log(a);
+
+
+// const b = 10; 
+
+// b = 20; //this is not ok
+
+// console.log(a);
