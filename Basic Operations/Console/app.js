94a2c893a08c00ec2da594263ea7ac546ec4ca1b
cagatay <drosen_26@hotmail.com>
2020-01-09 14:14:08 +0300

    https://github.com/cagataysen/JavaScript.git

---
+++ Basic Operations/Console/app.js
@@ -0,0 +1,18 @@
+console.log("Hello JS");
+console.log(21321321);
+console.log(true);
+console.log(false);
+
+
+var a = 20;
+
+console.log(a);
+
+console.log(["Cagatay", "Sen"]);
+
+console .log(typeof a);
+
+console.warn("this is a warning");
+console.error("this page can't found");
+console.clear();
+
